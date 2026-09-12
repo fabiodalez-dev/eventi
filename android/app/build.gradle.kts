314998589052d527019a307396e058698fd1ffe7
fabiodalez-dev <fabiodalezbackup@gmail.com>
2026-09-12 20:51:08 +0200

    chore: prepare Android 1.9.2 release

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "it.fabiodalez.incitta"
         minSdk = 26
         targetSdk = 36
-        versionCode = 23
-        versionName = "1.9.1"
+        versionCode = 24
+        versionName = "1.9.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
